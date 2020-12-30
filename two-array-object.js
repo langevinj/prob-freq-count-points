84fe9152cc51670f1ff9f47edd6047d9a5e29ba6
Jeremy Langevin <langevinj832@gmail.com>
2020-12-30 14:02:27 -0500

    Completed two-array-object

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,18 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keys, values) {
+    let obj = {};
+    let i = 0;
+
+    while(i < keys.length){
+        if(values[i]){
+            obj[keys[i]] = values[i]
+        } else  {
+            obj[keys[i]] = null
+        }
+        i++
+    }
+
+    return obj
 }
+
+module.exports = twoArrayObject;
