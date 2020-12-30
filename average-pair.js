2ed605cfd574f68668756bae8a21dab6679c8fdf
Jeremy Langevin <langevinj832@gmail.com>
2020-12-30 13:52:01 -0500

    Completed average-pair

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,20 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(array, target, left=0, right=array.length - 1) {
+
+    while(left < right){
+        let avg = (array[left] + array[right]) / 2.0
+
+        if(avg === target){
+            console.log(true)
+            return true;
+        } else if(avg < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return false
 }
+
+module.exports = averagePair;
