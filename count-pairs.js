d9e99ed5d2e49e44012873590aea769b5adb400b
Jeremy Langevin <langevinj832@gmail.com>
2020-12-31 08:22:33 -0500

    Completed count-pairs

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,12 @@
-// add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(array, target) {
+    let numPairs = 0;
+
+    while(array.length > 0){
+        let currVal = array.pop();
+        if(array.includes(target - currVal)) numPairs++
+    }
+
+    return numPairs;
 }
+
+module.exports = countPairs;
