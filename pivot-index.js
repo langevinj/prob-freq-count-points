497d7164ff0fe708f7955a2eb43e5f637d3b94a0
Jeremy Langevin <langevinj832@gmail.com>
2020-12-31 09:26:20 -0500

    Completed pivot-index

--- pivot-index.js
+++ pivot-index.js
@@ -1,3 +1,29 @@
-// add whatever parameters you deem necessary
-function pivotIndex() {
+
+function pivotIndex(array, mid=1) {
+
+    //sum a range between two indexes of an array
+    function sumRange(start, end){
+        let sum = 0;
+        for(let i=start; i < end; i++){
+            sum += array[i]
+        }
+        return sum
+    }   
+
+    //set beginning sums
+    let leftSum = array[0];
+
+    while(mid < array.length - 2){
+        let rightSum = sumRange(mid + 1, array.length)
+        if(leftSum === rightSum){
+            return mid;
+        }
+        //recalculate sum based on new range
+        mid++
+        leftSum = sumRange(0, mid);
+    }
+    
+    return -1;
 }
+
+module.exports = pivotIndex;
