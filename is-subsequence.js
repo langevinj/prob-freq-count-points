6ff3d0c046438ddb7fe710173ab3b500b1104c41
Jeremy Langevin <langevinj832@gmail.com>
2020-12-31 08:07:49 -0500

    Completed is-subsequence

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,17 @@
-// add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(str1, str2, left=0){
+    let str1Queue = str1.split("");
+    let currChar = str1Queue.shift();
+
+    while(left < str2.length){
+        if(str2[left] === currChar){
+            if(str1Queue.length === 0) return true
+            currChar = str1Queue.shift();
+        }
+
+        left++;
+    }
+
+    return false
 }
+
+module.exports = isSubsequence;
