7251edd657f1deef189811775e5f257c5b60a548
Jeremy Langevin <langevinj832@gmail.com>
2020-12-31 07:16:27 -0500

    Completed separate-positive

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,24 @@
-// add whatever parameters you deem necessary
-function separatePositive() {
+const isPositive = (currentValue) => currentValue < 0;
+const isNegative = (currentValue) => currentValue > 0;
+
+function separatePositive(array, left=0, right=array.length - 1){
+    //handle cases of all positive or negative arrays
+    if(array.every(isPositive) || array.every(isNegative)) return array;
+
+    while(left < right){
+        let mid = Math.floor((left + right) / 2);
+        let midVal = array.splice(mid, 1)[0];
+
+        if(midVal < 0){
+            array.push(midVal);
+            right--;
+        } else if (midVal > 0) {
+            array.unshift(midVal);
+            left++;
+        }
+    }
+
+    return array;
 }
+
+module.exports = separatePositive;
