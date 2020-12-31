61b60018b1c3b7a3b105748a68e951cd02cd8c42
Jeremy Langevin <langevinj832@gmail.com>
2020-12-31 08:37:57 -0500

    Completed longest-fall

--- longest-fall.js
+++ longest-fall.js
@@ -1,3 +1,22 @@
-// add whatever parameters you deem necessary
-function longestFall() {
+
+function longestFall(array, left=0, right=left+1, currLen=1) {
+    if(array.length === 0) return 0;
+    if(array.length === 1) return 1;
+    let tempLen = 1;
+
+    while(right < array.length){
+        if(array[left] > array[right] && array[right] < array[right - 1]){
+            //avoid duplicates interferring with count
+           if(array[right] !== array[right -1]) tempLen++;
+           currLen = tempLen > currLen ? tempLen : currLen
+        } else {
+          left++
+          tempLen=1;
+        }
+        right++;
+    }
+
+    return currLen;
 }
+
+module.exports = longestFall;
